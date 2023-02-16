7c286fcebe10f9bd8ae5ec4029547bbd27ddd4eb
xlordnoro <69708902+xlordnoro@users.noreply.github.com>
2023-02-16 01:02:57 -0500

    Typo correction + weapons_embed
    Fixed a small typo in the description where it says three instead of four in the jailer_embed.js. Added weapons_embed.js which handles all the responses for the choices when jailees fight for their life.

--- src/commands/tools/weapons_embed.js
+++ src/commands/tools/weapons_embed.js
@@ -18,21 +18,21 @@ const {
 
   module.exports = {
     data: new SlashCommandBuilder()
-      .setName("jailer_embed")
-      .setDescription("A one-way ticket to hell explained in autistic detail."),
+      .setName("weapons_embed")
+      .setDescription("A febal attempt to defend yourself from certain death."),
     async execute(interaction, client) {
       const { roles } = interaction.member;
       const role = await interaction.guild.roles
-        .fetch("1073461424577335327")
+        .fetch("1070213452397826092")
         .catch(console.error);
 
 //Cross-checks the fetch from earlier and if the user has the role, run the command. Otherwise, print a message to the user stating they lack the role required.
         
-      if (roles.cache.has("1073461424577335327")) {
+      if (roles.cache.has("1070213452397826092")) {
         const embed = new EmbedBuilder()
-          .setTitle(`Thank You, Devil Boy, and Welcome to Hell!`)
+          .setTitle(`May the odds be ever in your favor`)
           .setDescription(
-            `So, you thought you were safe by ignoring all of my warnings, did you? Well, I guess I shouldn't be surprised after being raised in a hell-hole kinda place like this for the last eighteen years of your life. Unfortunately, there will be no heroic escape, nor will you be saving any princesses in here. \n\nDepending on your reading speed, and if you have a functioning pair of eyes and a brain, you might be able to escape from here as I know about a  hidden path that the admins don't... Below are four choices, Devil Boy. Choose correctly and you'll find yourself on the other side of the gate to Hell. Choose incorrectly *heehee and I guess we'll be spending a lot of time together, Devil Boy.`
+            `Please select from the following options to protect yourself in battle.`
           )
           .setColor(14554646)
 
@@ -40,24 +40,19 @@ const {
 
         const buttons = new ActionRowBuilder().setComponents([
               new ButtonBuilder()
-                .setCustomId("darkness")
-                .setLabel("I'm not afraid of the darkness.")
+                .setCustomId("sword")
+                .setLabel("Sword")
+                .setStyle(ButtonStyle.Primary),
+  
+              new ButtonBuilder()
+                .setCustomId("staff")
+                .setLabel("Staff")
                 .setStyle(ButtonStyle.Secondary),
   
               new ButtonBuilder()
-                .setCustomId("serious")
-                .setLabel("Serious Time!!!!")
-                .setStyle(ButtonStyle.Secondary),
-  
-              new ButtonBuilder()
-                .setCustomId("changed_mind")
-                .setLabel("Okay, I've changed my ways.")
-                .setStyle(ButtonStyle.Secondary),
-  
-              new ButtonBuilder()
-                .setCustomId("twisted")
-                .setLabel("Really, I give up! Just make it stop already!")
-                .setStyle(ButtonStyle.Danger)
+                .setCustomId("bow")
+                .setLabel("Bow")
+                .setStyle(ButtonStyle.Success)
             ]
             );
 
@@ -70,8 +65,8 @@ const {
 
 //Sends the output of the embed to a different channel and pings the role via their id. Otherwise, print they lack the role required to run the command.
 
-        const channel = client.channels.cache.get('1073467097675477042');
-        channel.send({content: `<@&1073466795337453599>`, allowedMentions: { roles: ['1073466795337453599'] }, embeds: [embed], components: [buttons]});
+        const channel = client.channels.cache.get('1070431762196471888');
+        channel.send({content: `<@&1070214154926956584>`, allowedMentions: { roles: ['1070214154926956584'] }, embeds: [embed], components: [buttons]});
 
       } else {
         await interaction.reply({
